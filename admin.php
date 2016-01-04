06cbc270f98c4a890dc9a82e01b952fcba8ea853
Ben Redden <benjamminredden@gmail.com>
2016-01-04 15:38:15 -0500

    got oauth stuff into session stuff

--- admin.php
+++ admin.php
@@ -1,20 +1,15 @@
 <?php
 session_start();
 
-require __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 
-require __DIR__.'/conf.php';	session_start();
-
-	require __DIR__.'/vendor/autoload.php';
-	use phpish\shopify;
-
-	require __DIR__.'/conf.php';
+require_once __DIR__.'/conf.php';
 
 # shopify\access_token can throw an exception
 $oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
 
 $_SESSION['oauth_token'] = $oauth_token;
 $_SESSION['shop'] = $_GET['shop'];
-
+print_r($_SESSION);
 echo 'App Successfully Installed!';
